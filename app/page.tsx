2489762e03d9af6199969ebfee67b647aa92f8fc
Francisco <franciscoluna2004@gmail.com>
2026-05-03 11:32:38 -0400

    feat: set up octakit

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,17 @@
 import Image from "next/image";
+import { Octokit } from "@octokit/core";
 
-export default function Home() {
+export default async function Home() {
+const octokit = new Octokit({
+  auth: process.env.GITHUB_TOKEN,
+});
+
+const { data: pr } = await octokit.request("GET /repos/{owner}/{repo}", {
+  owner: "",
+  repo: "",
+});
+
+  console.log(pr);
   return (
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
